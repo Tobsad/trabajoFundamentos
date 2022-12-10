e0475faaf2e852bbca244e165bee9d5b0a98e6f8
Said Eslander <saideslander@gmail.com>
2022-12-10 10:30:46 -0500

    ejercicio4

---
+++ ejercicio4.py
@@ -0,0 +1,8 @@
+lista=["primer plato","plato 2","plato 3","plato 4","plato 5","plato 6","plato 7", "plato 8","ultimo plato"]
+
+cantidad=len(lista)
+# print(f"Elemento: {ultimo}")
+
+for i in range(cantidad):
+    n=-1    
+    print(f"Plato:", lista[n-i])
--- main.py
+++ main.py
@@ -5,3 +5,4 @@ cantidad=int(input("Ingrese la cantidad de elementos: "))
 for i in range(0,cantidad):
      print(f"Elemento {cantidad}")
      cantidad-=1
+     
